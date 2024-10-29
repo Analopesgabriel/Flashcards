fe9032ba685f39abafd5ec206b8951c58591e396
Ana Paual <163150083+Analopesgabriel@users.noreply.github.com>
2024-10-29 12:18:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  "Programação",
+  "O que é Python?",
+  "O Python é uma linguagem de programação."
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital de França?",
+    "Paris."
+  )
+
+  criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Um bloco de código que executa alguma tarefa."
+  )
+
+  criaCartao(
+    "Lingua Inglesa",
+    "Como se diz oi em Inglês?",
+    "Hi!"
+    )
